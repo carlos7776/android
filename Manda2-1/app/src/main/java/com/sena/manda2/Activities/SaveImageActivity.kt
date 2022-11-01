fc2976bf790b23e4dadbdd4a2fca213ad7618b1f
carlos7776 <cravila091@gmail.com>
2022-11-01 17:30:56 -0500

    user_rol_img

---
+++ Manda2-1/app/src/main/java/com/sena/manda2/Activities/SaveImageActivity.kt
@@ -0,0 +1,12 @@
+package com.sena.manda2.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sena.manda2.R
+
+class SaveImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_image)
+    }
+}
